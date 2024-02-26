7f18bd437a129f03a7907909aa3aab7748b9b4f9
ynotopec <49277296+ynotopec@users.noreply.github.com>
2024-02-26 13:22:38 +0100

    Update app.py

--- src/app.py
+++ src/app.py
@@ -5,67 +5,91 @@ from langchain_core.messages import AIMessage, HumanMessage
 from langchain_community.document_loaders import WebBaseLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
-from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+#from langchain_openai import OpenAIEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
+
+from langchain_openai import ChatOpenAI
+from langchain_community.llms import Ollama
+from langchain_community.chat_models import ChatLiteLLM
+
 from dotenv import load_dotenv
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain.chains import create_history_aware_retriever, create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 
+import os
+import torch
+os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
 
 load_dotenv()
+
+embeddings = HuggingFaceEmbeddings(model_name="OrdalieTech/Solon-embeddings-large-0.1")
+#model_name="dangvantuan/sentence-camembert-large")
 
 def get_vectorstore_from_url(url):
     # get the text in document form
     loader = WebBaseLoader(url)
     document = loader.load()
-    
+
     # split the document into chunks
-    text_splitter = RecursiveCharacterTextSplitter()
+    text_splitter = RecursiveCharacterTextSplitter(
+        separators=["\n\n", "\n", " ", ""],
+        chunk_size=396,
+        chunk_overlap=76,
+#        chunk_size=200,
+#        chunk_overlap=40,
+        length_function=len)
+#chunk_size=200)
     document_chunks = text_splitter.split_documents(document)
-    
+
     # create a vectorstore from the chunks
-    vector_store = Chroma.from_documents(document_chunks, OpenAIEmbeddings())
+    vector_store = Chroma.from_documents(document_chunks, embeddings)
+#OpenAIEmbeddings())
 
     return vector_store
 
 def get_context_retriever_chain(vector_store):
-    llm = ChatOpenAI()
-    
+    llm = ChatLiteLLM(model="ollama/mixtral", api_base=os.environ["OLLAMA_SERVER_URL"])
+    #llm = Ollama(model="mixtral")
+    #llm = ChatOpenAI()
+
     retriever = vector_store.as_retriever()
-    
+
     prompt = ChatPromptTemplate.from_messages([
       MessagesPlaceholder(variable_name="chat_history"),
       ("user", "{input}"),
       ("user", "Given the above conversation, generate a search query to look up in order to get information relevant to the conversation")
     ])
-    
+
     retriever_chain = create_history_aware_retriever(llm, retriever, prompt)
-    
+
     return retriever_chain
-    
-def get_conversational_rag_chain(retriever_chain): 
-    
-    llm = ChatOpenAI()
-    
+
+def get_conversational_rag_chain(retriever_chain):
+
+    llm = ChatLiteLLM(model="ollama/mixtral", api_base=os.environ["OLLAMA_SERVER_URL"])
+    #llm = ChatOpenAI()
+    #llm = Ollama(model="mixtral")
+
     prompt = ChatPromptTemplate.from_messages([
       ("system", "Answer the user's questions based on the below context:\n\n{context}"),
       MessagesPlaceholder(variable_name="chat_history"),
       ("user", "{input}"),
     ])
-    
+
     stuff_documents_chain = create_stuff_documents_chain(llm,prompt)
-    
+
     return create_retrieval_chain(retriever_chain, stuff_documents_chain)
 
 def get_response(user_input):
     retriever_chain = get_context_retriever_chain(st.session_state.vector_store)
     conversation_rag_chain = get_conversational_rag_chain(retriever_chain)
-    
+
     response = conversation_rag_chain.invoke({
         "chat_history": st.session_state.chat_history,
         "input": user_query
     })
-    
+
     return response['answer']
 
 # app config
@@ -87,7 +111,7 @@ else:
             AIMessage(content="Hello, I am a bot. How can I help you?"),
         ]
     if "vector_store" not in st.session_state:
-        st.session_state.vector_store = get_vectorstore_from_url(website_url)    
+        st.session_state.vector_store = get_vectorstore_from_url(website_url)
 
     # user input
     user_query = st.chat_input("Type your message here...")
@@ -95,8 +119,8 @@ else:
         response = get_response(user_query)
         st.session_state.chat_history.append(HumanMessage(content=user_query))
         st.session_state.chat_history.append(AIMessage(content=response))
-        
-       
+
+
 
     # conversation
     for message in st.session_state.chat_history:
